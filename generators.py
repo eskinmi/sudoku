2033268ea033b395e2bc0d560394defba1e45f11
mustafasamedeskin <meskin@wehkamp.nl>
2021-07-17 21:04:02 +0300

    generator solver all solutions find

--- generators.py
+++ generators.py
@@ -14,17 +14,17 @@ class GridGen(Grid):
     def _mk_empty_grid():
         return np.zeros((3, 3, 3, 3), dtype='int64').tolist()
 
-    def unit_generate(self):
+    def random_unit_generate(self):
         x,y = random.choice(self.solver.empty_boxes(self))
-        row = self.rows[x]
-        col = self.cols[y]
-        squ = self.solver.find_square(self, x, y)
-        values = self.solver.possible_values(row, col, squ)
+        values = self.solver.possible_values(self, x, y)
         self.insert(random.choice(values), x, y)
 
     def generate(self):
-        for ix in range(5):
-            self.unit_generate()
-            # if ix > 9:
-                # if self.solver.has_single_solution(self):
-                #     return Grid(self.G.tolist())
+        i = 0
+        n_elem = self.G.size
+        for ix in range(n_elem):
+            self.random_unit_generate()
+            if i > 17: # minimum elements that can make a sudoku
+                solutions = self.solver.find_all_solutions(self, max_solutions=2)
+                if len(solutions) == 1:
+                    return Grid(self.G.tolist())
--- solvers.py
+++ solvers.py
@@ -2,13 +2,14 @@ import numpy as np
 import itertools
 
 
-class BackTrackSolver():
+class BackTrackSolver:
 
     def __init__(self, max_depth=0):
         self.true_serie = np.arange(1, 10, 1)
         self.inserted = {}
         self.n_depth = 0
         self.max_depth = max_depth
+        self.puzzle = None
 
     @staticmethod
     def test(puzzle):
@@ -28,22 +29,26 @@ class BackTrackSolver():
     def find_square(puzzle, x, y):
         return puzzle.squares[int(np.floor(x / 3) * 3 + np.floor(y / 3))]
 
-    def possible_values(self, row, col, square):
-        return [i for i in self.true_serie if i not in col if i not in row if i not in square and i != 0]
+    def possible_values(self, puzzle, x, y):
+        return [i for i in self.true_serie
+                if i not in puzzle.cols[y]
+                if i not in puzzle.rows[x]
+                if i not in self.find_square(puzzle, x, y)
+                if i != 0]
 
     def solve(self, puzzle):
         self.n_depth += 1
         if self.max_depth and self.n_depth > self.max_depth:
             print('max recursion depth is reached!')
             return puzzle
-        empty_boxes = self.empty_boxes(puzzle)
-        if not empty_boxes:
+        boxes = self.empty_boxes(puzzle)
+        if not boxes:
+            self.puzzle = puzzle
             return puzzle
         else:
-            box = empty_boxes[0]
+            box = boxes[0]
             x, y = box[0], box[1]
-            squ = self.find_square(puzzle, x, y)
-            possible_vals = self.possible_values(puzzle.rows[x], puzzle.cols[y], squ)
+            possible_vals = self.possible_values(puzzle, x, y)
             for val in possible_vals:
                 puzzle.insert(val, x, y)
                 if self.test(puzzle):
@@ -54,5 +59,26 @@ class BackTrackSolver():
                     else:
                         puzzle.delete(x, y)
                 else:
-                    self.insert(0, x, y)
+                    puzzle.insert(0, x, y)
             return False
+
+    @staticmethod
+    def _grid_in_solutions(grid, grids):
+        return (grid == grids).all(axis=1).any()
+
+    def find_all_solutions(self, puzzle, max_solutions=5):
+        solutions = []
+        # add already tried solutions to the possible solutions
+        run = True
+        while run:
+            sol = self.solve(puzzle)
+            if solution:
+                if self._grid_in_solutions(sol.G, np.array(solutions)):
+                    run = False
+                else:
+                    solutions.append(sol.G)
+                if len(solutions) >= max_solutions:
+                    run = False
+            else:
+                run = False
+        return solutions
